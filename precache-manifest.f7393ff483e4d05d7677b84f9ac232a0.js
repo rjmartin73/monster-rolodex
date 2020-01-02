9e7e862fb968f38fa72e2c2f7f75941d11547c0b
Ryan Martin <rjmartin73@gmail.com>
2020-01-02 16:30:47 -0700

    Updates

---
+++ precache-manifest.f7393ff483e4d05d7677b84f9ac232a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9572a670835b646e518de0df36b88a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "348dbadf5a2b7e2aa480",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f6b31596fb729bc4a030",
+    "url": "/static/js/2.59166d94.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.59166d94.chunk.js.LICENSE"
+  },
+  {
+    "revision": "348dbadf5a2b7e2aa480",
+    "url": "/static/js/main.47ea9c00.chunk.js"
+  },
+  {
+    "revision": "66f4fd316769db8f5c69",
+    "url": "/static/js/runtime-main.3c711d1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
